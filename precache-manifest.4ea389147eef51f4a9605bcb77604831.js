4e49ec97e2ef65993454d9331508ffd23e265872
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-02-11 13:59:53 +0200

    Updates

---
+++ precache-manifest.4ea389147eef51f4a9605bcb77604831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4e4528d080ff73606ea39e2451d466",
+    "url": "./index.html"
+  },
+  {
+    "revision": "432191a6d450aa99f09f",
+    "url": "./static/css/main.d47585ca.chunk.css"
+  },
+  {
+    "revision": "5fdac0a2c9586d9d1584",
+    "url": "./static/js/2.6b91d4b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.6b91d4b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "432191a6d450aa99f09f",
+    "url": "./static/js/main.869a878a.chunk.js"
+  },
+  {
+    "revision": "e67b79006193623cc7c8",
+    "url": "./static/js/runtime-main.a19c704a.js"
+  }
+]);
